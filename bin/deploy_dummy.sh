9666c56c70fa1749169defd658ed5a6973c17db8
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-11 08:00:59 +0200

    Create deploy_dummy.sh

---
+++ bin/deploy_dummy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+function clean_up {
+    printf "\nCleaning up...\n"
+    rm -rf "${1}"
+}
+
+function error_exit {
+    printf "\n${1:-\"Unknown Error\"}\n" 1>&2
+    exit 1
+}
+
+printf "=====================================\n"
+printf "|        Demo  Deployment            |\n"
+printf "**This is a demo deployment example**\n"
+printf "=====================================\n"
+
+DIR="$(pwd)"
+file_path="${DIR}/src"
+file_name="test.txt"
+full_filename="${code_path}/${filename}"
+
+mkdir -p ${code_path}
+echo $(date +%s) > $full_filename
+
+# Clean up
+clean_up "${file_path}"
+printf "\nCompleted!\n"
+
+exit 0
