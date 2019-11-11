cea0ad756d0a0cfc2660f04c4ab41153d2294662
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-11 23:46:22 +0200

    Update pipeline_dummy.groovy

--- .jenkins/pipeline_dummy.groovy
+++ .jenkins/pipeline_dummy.groovy
@@ -37,7 +37,7 @@ pipeline
         stage('Build') {
             steps {
                 
-                docker.image('lambci/lambda:build-python3.7').inside('--user root -e AWS_REGION="${env.AWS_REGION}"'){
+                docker.image('lambci/lambda:build-python3.7').inside('--user root -e AWS_REGION="eu-west-1"'){
                     sh '''export AWS_SHARED_CREDENTIALS_FILE=/tmp/.aws
                         mkdir -p /tmp
                         touch ${AWS_SHARED_CREDENTIALS_FILE}
