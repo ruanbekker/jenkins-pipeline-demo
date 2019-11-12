d4b60fd07d0c5936736f99f2dd00e8ba3449cedd
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-12 12:24:58 +0200

    Update pipeline_dummy.groovy

--- .jenkins/pipeline_dummy.groovy
+++ .jenkins/pipeline_dummy.groovy
@@ -23,6 +23,7 @@ pipeline
         AWS_SECRET_KEY = credentials('AWS_MASTER_JENKINS_SK')
         AWS_CROSS_ACCOUNT_ROLE_ARN = "arn:aws:iam::$AWS_DEV_ACCOUNT_NUMBER:role/SystemCrossAccountAccess"
         GIT_TOKEN = credentials('GITHUB_TOKEN')
+        GITHUB_BRANCH_NAME = "master"
         SLACK_TOKEN_SECRET = credentials('SLACK_TOKEN_SECRET')
     }
     stages{
